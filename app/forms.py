bc46a5255705f7e43cf4aca03a99ffab33a5844f
Reeveo <lgcallow88@gmail.com>
2018-04-14 21:26:55 +0100

    Completed chap 3 webforms, included url_for and finished data validation

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, InputRequired, Email, NumberRange
+from wtforms.validators import DataRequired, InputRequired, Email, Length, EqualTo
 
 class LoginForm(FlaskForm):
-	username = StringField("Username", validators=[DataRequired("You need to enter you name"), NumberRange("2","Your full name...")])
-	email = StringField("Email", validators=[DataRequired("You need to enter something here"), Email("This filed required a valid email address.")])
+	username = StringField("Username", validators=[DataRequired(), Length(min=5,message="This needs to be at least 2 characters long")])
+	password = PasswordField("Password", validators=[DataRequired(), Length(5,message="It has got to be longer than that!")])
+	remember_me = BooleanField("Remember Me")
+	submit = SubmitField("Sign in")
+
+class RegistrationForm(FlaskForm):
+	username = StringField("Username", validators=[DataRequired(), Length(min=5,message="This needs to be at least 2 characters long")])
+	email = StringField("Email", validators=[DataRequired(), Email("This filed required a valid email address."),EqualTo('email2', "The passwords must match!")])
+	email2 = StringField("Email", validators=[DataRequired(), Email("This filed required a valid email address.")])
 	nickname = StringField("What would you prefer to be called?", validators=[])
-	password = PasswordField("Password", validators=[DataRequired(), NumberRange("5","Its got to be longer than that!")])
+	password = PasswordField("Password", validators=[DataRequired(), Length(5,message="Its got to be longer than that!")])
 	remember_me = BooleanField("Remember Me")
 	submit = SubmitField("Sign in")
