098c3699eb6055e4bc96f0df1b5d398a15b41d3c
Reeveo <lgcallow88@gmail.com>
2018-04-14 21:03:50 +0100

    linked up login hmtl to show validation errors

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -19,7 +19,7 @@ def index():
 	]
 	return render_template("index.html", title = "Home", user=user, posts = posts)
 
-@app.route("/login")	
+@app.route("/login", methods=["GET", "POST"])	
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
