05e4a00054c1adde5b55548e3f0b11b3906c75d8
Reeveo <lgcallow88@gmail.com>
2018-04-14 21:37:08 +0100

    Added registration page but not linked, ready for database input

--- app/forms.py
+++ app/forms.py
@@ -8,11 +8,4 @@ class LoginForm(FlaskForm):
 	remember_me = BooleanField("Remember Me")
 	submit = SubmitField("Sign in")
 
-class RegistrationForm(FlaskForm):
-	username = StringField("Username", validators=[DataRequired(), Length(min=5,message="This needs to be at least 2 characters long")])
-	email = StringField("Email", validators=[DataRequired(), Email("This filed required a valid email address."),EqualTo('email2', "The passwords must match!")])
-	email2 = StringField("Email", validators=[DataRequired(), Email("This filed required a valid email address.")])
-	nickname = StringField("What would you prefer to be called?", validators=[])
-	password = PasswordField("Password", validators=[DataRequired(), Length(5,message="Its got to be longer than that!")])
-	remember_me = BooleanField("Remember Me")
-	submit = SubmitField("Sign in")
+
