1f2cc61338dcfa741a34f96b4ed3b8ef532cfd39
Reeveo <33498623+Reeveo@users.noreply.github.com>
2018-04-16 16:59:04 +0100

    syntax error

--- app/routes.py
+++ app/routes.py
@@ -65,7 +65,7 @@ def user(username):
 	]
 	return render_template("user.html", user=user, posts=posts)
 
-@app.before.request
+@app.before_request
 def before_request:
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
