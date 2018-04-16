327ff4a55ee2995576614a97e0c79b7704a631ec
Reeveo <33498623+Reeveo@users.noreply.github.com>
2018-04-16 10:39:01 +0100

    added Registration form and validators

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, InputRequired, Email, Length, EqualTo
+from wtforms.validators import DataRequired, InputRequired, Email, Length, EqualTo, ValidationError
+from app.models import User
 
+	
 class LoginForm(FlaskForm):
-	username = StringField("Username", validators=[DataRequired(), Length(min=5,message="This needs to be at least 2 characters long")])
+	username = StringField("Username", validators=[DataRequired(), Length(min=5,message="This needs to be at least 5 characters long")])
 	password = PasswordField("Password", validators=[DataRequired(), Length(5,message="It has got to be longer than that!")])
 	remember_me = BooleanField("Remember Me")
 	submit = SubmitField("Sign in")
 
-
+class RegistrationForm(FlaskForm):
+	username = StringField("Username", validators=[DataRequired(), Length(min=5,message="This needs to be at least 5 characters long")])
+	email = StringField("Email", validators=[DataRequired(), Email()])
+	password = PasswordField("Password", validators=[DataRequired()])
+	password2 = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo("password")])
+	submit = SubmitField("Register")
+	
+	def validate_username(self, username):
+		user = User.query.filter_by(username = username.data).first()
+		if user is not None:
+			raise ValidationError("Please use a different username")
+	
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user is not None:
+			raise ValidationError("Please use a different email address")
+			
