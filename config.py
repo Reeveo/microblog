ba9cfff64791fc7428f3483318cd1befab8038fe
Reeveo <33498623+Reeveo@users.noreply.github.com>
2018-04-16 11:22:53 +0100

    changed session cookie to one day

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
+from datetime import timedelta
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    # ...
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PERMANENT_SESSION_LIFETIME = timedelta(days=1)
+    REMEMBER_COOKIE_DURATION = timedelta(days=1)
