ab4709587e1427e32c17b89ddb6f032b57177237
Reeveo <33498623+Reeveo@users.noreply.github.com>
2018-04-16 09:49:51 +0100

    changed set password functions

--- app/models.py
+++ app/models.py
@@ -13,8 +13,8 @@ class User(Usermixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-    def set_password(self, pasword):
-        self.password_hash = generate_password_hash(set_password)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
