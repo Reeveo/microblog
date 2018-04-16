394c2949129d49822ce50c7f5dd3dcc675b8fe4a
Reeveo <33498623+Reeveo@users.noreply.github.com>
2018-04-16 16:29:20 +0100

    added about me and last seen columns

--- app/models.py
+++ app/models.py
@@ -16,6 +16,8 @@ class User(Usermixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.datetime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
         # This repr allows you to use flask shell to print the username - also used below for posts
