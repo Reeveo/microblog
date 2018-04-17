73ea06c8378094ee59671fdd29bab5adea085bbd
Reeveo <33498623+Reeveo@users.noreply.github.com>
2018-04-17 08:32:28 +0100

    missing brackets on functions

--- app/routes.py
+++ app/routes.py
@@ -66,14 +66,14 @@ def user(username):
 	return render_template("user.html", user=user, posts=posts)
 
 @app.before_request
-def before_request:
+def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
 @app.route("edit_profile/", methods=["GET", "POST"])
 @login_required
-def edit_profile:
+def edit_profile():
     form = EditProfileForm()
     if form.validate_on_submit():
         current_user.username = form.username.data
