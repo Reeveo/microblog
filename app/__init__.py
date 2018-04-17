9302d8099025bf66e6106f560a0dba50d0a7cf2b
Reeveo <33498623+Reeveo@users.noreply.github.com>
2018-04-17 09:12:18 +0100

    added logger info

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+# logging package writes logs and this is used to send error reports to the admin email
+import logging
+from logging.handlers import SMTPHandler
 
 
 app = Flask(__name__)
@@ -20,3 +23,18 @@ login.login_view = 'login'
 #Need to import the below to get them registered with Flask
 from app import routes, models, errors
 
+if not app.debug:
+    if app.config["MAIL_SERVER"]:
+        auth = None
+        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+            auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+        secure = None
+        if app.config["MAIL_USE_TLS"]:
+            secure()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"])
+            fromaddr="no-reply@" + app.config["MAIL_SERVER"]
+            toaddrs= app.config["ADMINS"], subject="Microblog Failure",
+            credentials= auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
