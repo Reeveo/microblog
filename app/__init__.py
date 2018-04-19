d0a74596605d427ccfd3ebc4c9d12b145bd9c426
Reeveo <lgcallow88@gmail.com>
2018-04-19 11:35:12 +0100

    Changes

--- app/__init__.py
+++ app/__init__.py
@@ -29,7 +29,7 @@ if not app.debug:
             auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
         secure = None
         if app.config["MAIL_USE_TLS"]:
-            secure()
+            secure = ()
         mail_handler = SMTPHandler(mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),fromaddr="no-reply@" + app.config["MAIL_SERVER"], toaddrs= app.config["ADMINS"], subject="Microblog Failure", credentials= auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
