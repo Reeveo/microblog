077e2495fa39a4265390edbec59f90d95869e592
Reeveo <lgcallow88@gmail.com>
2018-04-20 17:57:17 +0100

    tests added amd followers V2

--- app/models.py
+++ app/models.py
@@ -54,8 +54,26 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode("utf-8")).hexdigest()
         return "https://www.gravatar.com/avatar/{}?d=identicon&s={}".format(digest, size)
     
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+        
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
     
-    
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
+        
     # Datetime is used to timestampe when the post is created which allows for filtering etc. UTC specifically to avoid time conflicts
     #Set up the Post table
 class Post(db.Model):
