5f1023ac453f96d5482fdf33d92e015010c18a12
Reeveo <lgcallow88@gmail.com>
2018-04-24 11:21:33 +0100

    finished pagnation

--- app/routes.py
+++ app/routes.py
@@ -17,7 +17,7 @@ def before_request():
 @app.route("/index", methods=["GET", "POST"])
 @login_required
 def index():
-	form= PostForm()
+    form= PostForm()
     if form.validate_on_submit():
         post= Post(body=form.post.data, author= current_user)
         db.session.add(post)
@@ -25,7 +25,16 @@ def index():
         flash("Your post is now live!")
         return redirect(url_for("index"))
     posts = current_user.followed_posts().all()
-	return render_template("index.html", title= "Home Page", posts= posts)
+    page= request.args.get("page",1, type=int)
+    posts= current_user.followed_posts().paginate(
+        page, app.config["POSTS_PER_PAGE"], False)
+    next_url= url_for("index",page=posts.next_num) \
+        if posts.has_next else None
+    prev_url= url_for("index",page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template("index.html", title= "Home Page", 
+                            form=form, posts= posts.items, next_url=next_url,
+                            prev_url=prev_url)
 
 '''
 @login_required decorator will intercept the request and respond with a redirect to /login,
@@ -72,12 +81,16 @@ def register():
 @app.route("/user/<username>")
 @login_required
 def user(username):
-	user = User.query.filter_by(username=username).first_or_404()
-	posts = [
-		{"author":user, "body":"Test post #1"},
-		{"author":user, "body":"Test post #2"}
-	]
-	return render_template("user.html", user=user, posts=posts)
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 
@@ -129,5 +142,7 @@ def unfollow(username):
 @app.route("/explore")
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template("index.html", title="Explore", posts= posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+    page, app.config['POSTS_PER_PAGE'], False)
+    return render_template("index.html", title="Explore", posts= posts.items)
